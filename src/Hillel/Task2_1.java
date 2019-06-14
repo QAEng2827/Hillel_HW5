964fd698906c1ff61f5f891a9f351b140b6366e9
QAEng2827 <inna.nakopyuk@gmail.com>
2019-06-14 13:06:45 +0300

    HW5 Task1-1 .gitignore +task2_1 (complicate)

--- src/Hillel/Task2_1.java
+++ src/Hillel/Task2_1.java
@@ -1,36 +1,48 @@
 package Hillel;
-/* Create an array of double and fill( ) it using RandDoubleGenerator. Print the results.*/
+/* Create an array of double and fillArray( ) it using RandDoubleGenerator. Print the results.*/
 
 import java.util.Random;
 
-//import  com.bruceeckel.util.*;
+
 public class Task2_1 {
+    private static double[] dArr;
 
-//    public void double randDoubleGenetration(){
-//
-//    }
-    public static void main(String args[]){
-
-//        public static void main(String args[]) {
-//            double[] da = new double[10];
-//
-//            Arrays2.fill(da, new Arrays2.RandDoubleGenerator());
-//            System.out.println(Arrays2.toString(da));
-//        }
+    public static double[] fillArray(){
+        dArr = new double[10];
+        for (int i = 0; i< dArr.length; i++) {
+            dArr[i] = randDoubleGenetration();
 
 
+        }
+
+        return dArr;
+    }
+
+    public static double randDoubleGenetration(){
         Random rand = new Random();
-
-    double[] dArr = new double[10];
-
-    for (int i = 0; i< dArr.length; i++) {
-        dArr[i] = rand.nextDouble();
-        System.out.println(dArr[i]);
-
- //       Arrays.fill(dArr,dArr[i]);
+        double randDoub=rand.nextDouble();
+  return randDoub;
 
     }
-  //      System.out.println(Arrays.toString(dArr));
-}
 
-}
+
+    public static void print(){
+        for(double n: dArr){
+            System.out.println(n+" ");
+        }
+    }
+
+
+    public static void main(String args[]) {
+        fillArray();
+        print();
+
+    }
+
+
+
+
+ //       Arrays.fillArray(dArr,dArr[i]);
+
+    }
+
